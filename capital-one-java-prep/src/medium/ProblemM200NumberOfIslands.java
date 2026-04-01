dbfb7b47032f61323d9c469296deb666449f1d75
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 19:58:51 -0400

    Refactor numIslands method and update dfs logic

--- capital-one-java-prep/src/medium/ProblemM200NumberOfIslands.java
+++ capital-one-java-prep/src/medium/ProblemM200NumberOfIslands.java
@@ -1,25 +1,29 @@
-public class ProblemM200NumberOfIslands {
+class Solution {
     public int numIslands(char[][] grid) {
-        int answer = 0;
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == '1') {
-                    answer++;
-                    dfs(grid, r, c);
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return answer;
+
+        return count;
     }
 
-    private void dfs(char[][] grid, int r, int c) {
-        if (r < 0 || c < 0 || r == grid.length || c == grid[0].length || grid[r][c] != '1') {
+    private void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0') {
             return;
         }
-        grid[r][c] = '0';
-        dfs(grid, r + 1, c);
-        dfs(grid, r - 1, c);
-        dfs(grid, r, c + 1);
-        dfs(grid, r, c - 1);
+
+        grid[row][col] = '0';
+
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
     }
 }
