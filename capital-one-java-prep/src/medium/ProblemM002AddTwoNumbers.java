6c5813b80c98bc1529663fb54ad488799cb83db0
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 19:48:34 -0400

    Refactor addTwoNumbers method for clarity

--- capital-one-java-prep/src/medium/ProblemM002AddTwoNumbers.java
+++ capital-one-java-prep/src/medium/ProblemM002AddTwoNumbers.java
@@ -1,27 +1,27 @@
-public class ProblemM002AddTwoNumbers {
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
+class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+      ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
         int carry = 0;
 
-        while (l1 != null || l2 != null || carry > 0) {
-            int a = l1 != null ? l1.val : 0;
-            int b = l2 != null ? l2.val : 0;
-            int sum = a + b + carry;
-            tail.next = new ListNode(sum % 10);
-            tail = tail.next;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
             carry = sum / 10;
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
         }
+
         return dummy.next;
     }
 }
