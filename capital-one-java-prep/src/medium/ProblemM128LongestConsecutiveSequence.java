3346760fadac0eff89d5a1862e06eca0c13927d1
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 19:57:21 -0400

    Refactor longestConsecutive method implementation
    Refactor longestConsecutive method to use sorting instead of a set for finding the longest consecutive sequence.

--- capital-one-java-prep/src/medium/ProblemM128LongestConsecutiveSequence.java
+++ capital-one-java-prep/src/medium/ProblemM128LongestConsecutiveSequence.java
@@ -1,22 +1,25 @@
-import java.util.*;
-
-public class ProblemM128LongestConsecutiveSequence {
+class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) set.add(num);
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] actualNums = nums;
+        Arrays.sort(actualNums);
+        int maxLength = 0;
+        int count = 0;
+        for(int i=0; i<nums.length -1 ; i++) {
+            if(actualNums[i] + 1 == actualNums[i+1]) {                
+                count++;
+            } else if (actualNums[i] == actualNums[i+1]) {
 
-        int answer = 0;
-        for (int num : set) {
-            if (!set.contains(num - 1)) {
-                int current = num;
-                int length = 1;
-                while (set.contains(current + 1)) {
-                    current++;
-                    length++;
-                }
-                answer = Math.max(answer, length);
+            }
+            else {
+                if(count > maxLength) {
+                    maxLength = count;
+                }                
+                count = 0;
             }
         }
-        return answer;
+        return count > maxLength ? count +1  : maxLength + 1;
     }
 }
