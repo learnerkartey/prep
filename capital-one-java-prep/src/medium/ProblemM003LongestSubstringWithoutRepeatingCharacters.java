859f396df10a21b24eaabbde5afc98ff55188897
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 19:49:36 -0400

    Refactor lengthOfLongestSubstring method for efficiency

--- capital-one-java-prep/src/medium/ProblemM003LongestSubstringWithoutRepeatingCharacters.java
+++ capital-one-java-prep/src/medium/ProblemM003LongestSubstringWithoutRepeatingCharacters.java
@@ -1,19 +1,17 @@
-import java.util.*;
-
-public class ProblemM003LongestSubstringWithoutRepeatingCharacters {
+class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> lastSeen = new HashMap<>();
+         int[] lastSeen = new int[128];
+        Arrays.fill(lastSeen, -1);
         int left = 0;
-        int answer = 0;
-
+        int maxLen = 0;
         for (int right = 0; right < s.length(); right++) {
             char ch = s.charAt(right);
-            if (lastSeen.containsKey(ch)) {
-                left = Math.max(left, lastSeen.get(ch) + 1);
+            if (lastSeen[ch] >= left) {
+                left = lastSeen[ch] + 1;
             }
-            lastSeen.put(ch, right);
-            answer = Math.max(answer, right - left + 1);
+            lastSeen[ch] = right;
+            maxLen = Math.max(maxLen, right - left + 1);
         }
-        return answer;
+        return maxLen;
     }
 }
