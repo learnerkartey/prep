9885055c701af74e3183652b946d0d6683bf7c3a
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 19:39:15 -0400

    Refactor isMonotonic method for clarity

--- capital-one-java-prep/src/easy/ProblemE896MonotonicArray.java
+++ capital-one-java-prep/src/easy/ProblemE896MonotonicArray.java
@@ -1,11 +1,17 @@
-public class ProblemE896MonotonicArray {
+class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean nonDecreasing = true;
-        boolean nonIncreasing = true;
+        boolean increasing = true;
+        boolean decreasing = true;
+
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < nums[i - 1]) nonDecreasing = false;
-            if (nums[i] > nums[i - 1]) nonIncreasing = false;
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            }
+            if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
         }
-        return nonDecreasing || nonIncreasing;
+
+        return increasing || decreasing;
     }
 }
