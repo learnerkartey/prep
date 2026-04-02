9318ad6d5ee8a57a4bf9e9152b04928c6074f118
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:13:22 -0400

    Refactor SimpleBankSystem into Bank class

--- capital-one-java-prep/src/medium/ProblemM2043SimpleBankSystem.java
+++ capital-one-java-prep/src/medium/ProblemM2043SimpleBankSystem.java
@@ -1,30 +1,52 @@
-public class ProblemM2043SimpleBankSystem {
-    private final long[] balance;
-
-    public ProblemM2043SimpleBankSystem(long[] balance) {
-        this.balance = balance;
+class Bank {
+    long[] accountBalance;
+    int totalAccount;
+    public Bank(long[] balance) {
+        this.accountBalance = balance;
+        this.totalAccount = balance.length;
     }
 
+    private boolean isValidAccount(int acccount) {
+        return acccount <= totalAccount && acccount > 0;
+    }
+
+    private boolean hasBalance(int account, long money) {
+        if(!isValidAccount(account)) {
+            return false;
+        }
+        return this.accountBalance[account - 1] >= money;
+    }
+    
     public boolean transfer(int account1, int account2, long money) {
-        if (!valid(account1) || !valid(account2) || balance[account1 - 1] < money) return false;
-        balance[account1 - 1] -= money;
-        balance[account2 - 1] += money;
-        return true;
+        if(hasBalance(account1, money) && isValidAccount(account2) && money >= 0) {
+            this.accountBalance[account2 - 1] += money;
+            this.accountBalance[account1 - 1] -= money;
+            return true;
+        }
+        return false;
     }
-
+    
     public boolean deposit(int account, long money) {
-        if (!valid(account)) return false;
-        balance[account - 1] += money;
-        return true;
+        if(money >= 0 && isValidAccount(account)) {
+            this.accountBalance[account - 1] += money;
+            return true;
+        }
+        return false;
     }
-
+    
     public boolean withdraw(int account, long money) {
-        if (!valid(account) || balance[account - 1] < money) return false;
-        balance[account - 1] -= money;
-        return true;
-    }
-
-    private boolean valid(int account) {
-        return account >= 1 && account <= balance.length;
+        if(hasBalance(account, money)) {
+            this.accountBalance[account - 1] -= money;
+            return true;
+        }
+        return false;
     }
 }
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
