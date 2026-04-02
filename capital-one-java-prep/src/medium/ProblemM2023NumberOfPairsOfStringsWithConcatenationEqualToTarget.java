aaf87deaf978cf563f8d59e421185885b062438f
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:12:54 -0400

    Refactor numOfPairs method for clarity and correctness

--- capital-one-java-prep/src/medium/ProblemM2023NumberOfPairsOfStringsWithConcatenationEqualToTarget.java
+++ capital-one-java-prep/src/medium/ProblemM2023NumberOfPairsOfStringsWithConcatenationEqualToTarget.java
@@ -1,21 +1,13 @@
-import java.util.*;
-
-public class ProblemM2023NumberOfPairsOfStringsWithConcatenationEqualToTarget {
+class Solution {
     public int numOfPairs(String[] nums, String target) {
-        Map<String, Integer> count = new HashMap<>();
-        for (String num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
-
-        int answer = 0;
-        for (int i = 1; i < target.length(); i++) {
-            String left = target.substring(0, i);
-            String right = target.substring(i);
-            if (!count.containsKey(left) || !count.containsKey(right)) continue;
-            if (left.equals(right)) {
-                answer += count.get(left) * (count.get(left) - 1);
-            } else {
-                answer += count.get(left) * count.get(right);
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && (nums[i] + nums[j]).equals(target)) {
+                    count++;
+                }
             }
         }
-        return answer;
+        return count;
     }
 }
