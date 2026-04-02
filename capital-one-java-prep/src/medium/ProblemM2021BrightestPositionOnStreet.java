36b3753ca2b17360caf40983c73765f14d9b0bb1
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:12:03 -0400

    Refactor brightestPosition method for clarity

--- capital-one-java-prep/src/medium/ProblemM2021BrightestPositionOnStreet.java
+++ capital-one-java-prep/src/medium/ProblemM2021BrightestPositionOnStreet.java
@@ -1,25 +1,28 @@
-import java.util.*;
-
-public class ProblemM2021BrightestPositionOnStreet {
+class Solution {
     public int brightestPosition(int[][] lights) {
-        TreeMap<Integer, Integer> diff = new TreeMap<>();
+         TreeMap<Integer, Integer> sweep = new TreeMap<>();
+
         for (int[] light : lights) {
             int start = light[0] - light[1];
             int end = light[0] + light[1];
-            diff.put(start, diff.getOrDefault(start, 0) + 1);
-            diff.put(end + 1, diff.getOrDefault(end + 1, 0) - 1);
+
+            sweep.put(start, sweep.getOrDefault(start, 0) + 1);
+            sweep.put(end + 1, sweep.getOrDefault(end + 1, 0) - 1);
         }
 
-        int bestPos = 0;
-        int best = Integer.MIN_VALUE;
-        int current = 0;
-        for (Map.Entry<Integer, Integer> entry : diff.entrySet()) {
-            current += entry.getValue();
-            if (current > best) {
-                best = current;
-                bestPos = entry.getKey();
+        int brightness = 0;
+        int maxBrightness = 0;
+        int answer = 0;
+
+        for (Map.Entry<Integer, Integer> entry : sweep.entrySet()) {
+            brightness += entry.getValue();
+
+            if (brightness > maxBrightness) {
+                maxBrightness = brightness;
+                answer = entry.getKey();
             }
         }
-        return bestPos;
+
+        return answer;
     }
 }
