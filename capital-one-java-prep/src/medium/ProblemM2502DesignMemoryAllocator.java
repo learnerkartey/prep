3f87e9a9aa056908cbb857abef0f799aeafe3c78
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:13:47 -0400

    Refactor ProblemM2502DesignMemoryAllocator to Allocator

--- capital-one-java-prep/src/medium/ProblemM2502DesignMemoryAllocator.java
+++ capital-one-java-prep/src/medium/ProblemM2502DesignMemoryAllocator.java
@@ -1,32 +1,49 @@
-public class ProblemM2502DesignMemoryAllocator {
+class Allocator {
+    
     private final int[] memory;
 
-    public ProblemM2502DesignMemoryAllocator(int n) {
-        this.memory = new int[n];
+    public Allocator(int n) {
+        memory = new int[n];
+        for(int i=0;i<memory.length; i++) {
+            memory[i]= 0;
+        }
     }
-
+    
     public int allocate(int size, int mID) {
-        int free = 0;
-        for (int i = 0; i < memory.length; i++) {
-            if (memory[i] == 0) free++;
-            else free = 0;
-            if (free == size) {
-                int start = i - size + 1;
-                for (int j = start; j <= i; j++) memory[j] = mID;
-                return start;
+        int count =0;
+        for(int i=0; i<memory.length; i++) {
+            if(memory[i] == 0) {
+                count++;
+            } else {
+                count = 0;
             }
+
+            if(count >= size) {
+                int startIndex = i - size+1;
+                for(int j=startIndex; j<= i; j++) {
+                    memory[j] = mID;
+                }
+                return startIndex;
+            }            
         }
         return -1;
     }
-
+    
     public int freeMemory(int mID) {
         int freed = 0;
-        for (int i = 0; i < memory.length; i++) {
-            if (memory[i] == mID) {
+        for(int i=0; i<memory.length; i++) {
+            if(memory[i] == mID) {
                 memory[i] = 0;
                 freed++;
-            }
+            }            
         }
         return freed;
     }
 }
+
+/**
+ * Your Allocator object will be instantiated and called as such:
+ * Allocator obj = new Allocator(n);
+ * int param_1 = obj.allocate(size,mID);
+ * int param_2 = obj.freeMemory(mID);
+ */
