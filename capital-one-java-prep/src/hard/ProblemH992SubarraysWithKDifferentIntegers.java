ffc4a9cb9c352258543f5a0d806113843f3989fe
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:20:14 -0400

    Update ProblemH992SubarraysWithKDifferentIntegers.java

--- capital-one-java-prep/src/hard/ProblemH992SubarraysWithKDifferentIntegers.java
+++ capital-one-java-prep/src/hard/ProblemH992SubarraysWithKDifferentIntegers.java
@@ -1,25 +1,27 @@
-import java.util.*;
-
-public class ProblemH992SubarraysWithKDifferentIntegers {
+class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
         return atMost(nums, k) - atMost(nums, k - 1);
     }
 
     private int atMost(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
+        Map<Integer, Integer> freq = new HashMap<>();
         int left = 0;
-        int answer = 0;
+        int count = 0;
 
         for (int right = 0; right < nums.length; right++) {
-            count.put(nums[right], count.getOrDefault(nums[right], 0) + 1);
-            while (count.size() > k) {
-                int value = nums[left++];
-                int freq = count.get(value) - 1;
-                if (freq == 0) count.remove(value);
-                else count.put(value, freq);
+            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+
+            while (freq.size() > k) {
+                freq.put(nums[left], freq.get(nums[left]) - 1);
+                if (freq.get(nums[left]) == 0) {
+                    freq.remove(nums[left]);
+                }
+                left++;
             }
-            answer += right - left + 1;
+
+            count += right - left + 1;
         }
-        return answer;
+
+        return count;
     }
 }
