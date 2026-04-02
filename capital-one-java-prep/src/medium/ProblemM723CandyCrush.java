627870b1ef0f5afe5aaff087a8de956c6c9de666
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:03:54 -0400

    Refactor candyCrush method and related functions

--- capital-one-java-prep/src/medium/ProblemM723CandyCrush.java
+++ capital-one-java-prep/src/medium/ProblemM723CandyCrush.java
@@ -1,52 +1,46 @@
-public class ProblemM723CandyCrush {
+class Solution {
     public int[][] candyCrush(int[][] board) {
-        int rows = board.length;
-        int cols = board[0].length;
-        boolean changed = true;
-
-        while (changed) {
-            changed = false;
-
-            for (int r = 0; r < rows; r++) {
-                for (int c = 0; c + 2 < cols; c++) {
-                    int v = Math.abs(board[r][c]);
-                    if (v != 0 && Math.abs(board[r][c + 1]) == v && Math.abs(board[r][c + 2]) == v) {
-                        int k = c;
-                        while (k < cols && Math.abs(board[r][k]) == v) {
-                            board[r][k] = -v;
-                            k++;
-                        }
-                        changed = true;
-                    }
+        if(board.length == 0) return board;        
+        int crushed = crush(board);
+        while(crushed > 0){
+            dropUncrushed(board);
+            crushed = crush(board);
+        }        
+        return board;
+    }    
+    private int crush(int[][] board){
+        Set<int[]> set = new HashSet<>();        
+        for(int i=0; i < board.length; i++){
+            for(int j=2; j < board[0].length; j++){
+                if(board[i][j-2] == board[i][j-1] && board[i][j-1] == board[i][j] && board[i][j] != 0){
+                    set.addAll(Set.of(new int[]{i, j-2}, new int[]{i, j-1}, new int[]{i, j}));
                 }
             }
-
-            for (int c = 0; c < cols; c++) {
-                for (int r = 0; r + 2 < rows; r++) {
-                    int v = Math.abs(board[r][c]);
-                    if (v != 0 && Math.abs(board[r + 1][c]) == v && Math.abs(board[r + 2][c]) == v) {
-                        int k = r;
-                        while (k < rows && Math.abs(board[k][c]) == v) {
-                            board[k][c] = -v;
-                            k++;
-                        }
-                        changed = true;
-                    }
+        }        
+        for(int j=0; j < board[0].length; j++){
+            for(int i=2; i < board.length; i++){
+                if(board[i-2][j] == board[i-1][j] && board[i-1][j] == board[i][j] && board[i][j] != 0){
+                    set.addAll(Set.of(new int[]{i-2, j}, new int[]{i-1, j}, new int[]{i, j}));
                 }
             }
-
-            if (changed) {
-                for (int c = 0; c < cols; c++) {
-                    int write = rows - 1;
-                    for (int r = rows - 1; r >= 0; r--) {
-                        if (board[r][c] > 0) {
-                            board[write--][c] = board[r][c];
-                        }
-                    }
-                    while (write >= 0) board[write--][c] = 0;
+        }        
+        for(int[] p: set){
+            board[p[0]][p[1]] = 0;
+        }        
+        return set.size();
+    }    
+    private void dropUncrushed(int[][] board){
+        for(int j=0; j < board[0].length; j++){
+            
+            int offset = 0;
+            for(int i=board.length-1; i >= 0; i--){
+                if(board[i][j] == 0){
+                    offset++;
+                }else{
+                    board[i+offset][j] = board[i][j];
+                    board[i][j] = (offset > 0) ? 0 : board[i][j];
                 }
             }
         }
-        return board;
     }
 }
