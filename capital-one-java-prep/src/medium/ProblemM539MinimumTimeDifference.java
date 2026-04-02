0df91d87bc0947fb13f29e02045f52c02dd8b34f
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:01:40 -0400

    Update ProblemM539MinimumTimeDifference.java

--- capital-one-java-prep/src/medium/ProblemM539MinimumTimeDifference.java
+++ capital-one-java-prep/src/medium/ProblemM539MinimumTimeDifference.java
@@ -1,19 +1,19 @@
-import java.util.*;
-
-public class ProblemM539MinimumTimeDifference {
+class Solution {
     public int findMinDifference(List<String> timePoints) {
         List<Integer> minutes = new ArrayList<>();
+
         for (String time : timePoints) {
-            int h = Integer.parseInt(time.substring(0, 2));
-            int m = Integer.parseInt(time.substring(3, 5));
-            minutes.add(h * 60 + m);
+            int hour = Integer.parseInt(time.substring(0, 2));
+            int minute = Integer.parseInt(time.substring(3, 5));
+            minutes.add(hour * 60 + minute);
         }
         Collections.sort(minutes);
-
-        int answer = 1440 - minutes.get(minutes.size() - 1) + minutes.get(0);
+        int minDiff = Integer.MAX_VALUE;
         for (int i = 1; i < minutes.size(); i++) {
-            answer = Math.min(answer, minutes.get(i) - minutes.get(i - 1));
+            minDiff = Math.min(minDiff, minutes.get(i) - minutes.get(i - 1));
         }
-        return answer;
+        int circularDiff = 1440 - minutes.get(minutes.size() - 1) + minutes.get(0);
+        minDiff = Math.min(minDiff, circularDiff);
+        return minDiff;
     }
 }
