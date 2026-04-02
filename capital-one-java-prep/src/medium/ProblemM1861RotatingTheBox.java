f421857f72208e4774e16b56bae9e00a54b8cba3
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:11:30 -0400

    Refactor rotateTheBox method for clarity and efficiency

--- capital-one-java-prep/src/medium/ProblemM1861RotatingTheBox.java
+++ capital-one-java-prep/src/medium/ProblemM1861RotatingTheBox.java
@@ -1,27 +1,29 @@
-public class ProblemM1861RotatingTheBox {
+class Solution {
+    // BruteForce Solution
     public char[][] rotateTheBox(char[][] box) {
-        int rows = box.length;
-        int cols = box[0].length;
+        int row = box.length, col = box[0].length;
+        char[][] box_90 = new char[col][row];
 
-        for (int r = 0; r < rows; r++) {
-            int write = cols - 1;
-            for (int c = cols - 1; c >= 0; c--) {
-                if (box[r][c] == '*') {
-                    write = c - 1;
-                } else if (box[r][c] == '#') {
-                    box[r][c] = '.';
-                    box[r][write] = '#';
-                    write--;
+        // Simulate gravity
+        for (int i = 0; i < row; i++) {
+            int cell = col - 1;
+            for (int j = col - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    cell = j - 1;
+                } else if (box[i][j] == '#') {
+                    box[i][j] = '.';
+                    box[i][cell--] = '#';
                 }
             }
         }
 
-        char[][] rotated = new char[cols][rows];
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                rotated[c][rows - 1 - r] = box[r][c];
+        // Rotate the box 90 degrees clockwise
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                box_90[j][row - i - 1] = box[i][j];
             }
         }
-        return rotated;
+
+        return box_90;
     }
 }
