73fa7a35b21e3520f0ae189335242c495ee53f70
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:19:50 -0400

    Update ProblemH546RemoveBoxes.java

--- capital-one-java-prep/src/hard/ProblemH546RemoveBoxes.java
+++ capital-one-java-prep/src/hard/ProblemH546RemoveBoxes.java
@@ -1,34 +1,35 @@
-public class ProblemH546RemoveBoxes {
-    private int[][][] memo;
-    private int[] boxes;
-
+class Solution {
     public int removeBoxes(int[] boxes) {
-        int n = boxes.length;
-        this.boxes = boxes;
-        this.memo = new int[n][n][n];
-        return dfs(0, n - 1, 0);
+     int n = boxes.length;
+        int[][][] dp = new int[n][n][n];
+        return dfs(boxes, 0, n - 1, 0, dp);
     }
 
-    private int dfs(int left, int right, int sameCount) {
-        if (left > right) return 0;
-        if (memo[left][right][sameCount] != 0) return memo[left][right][sameCount];
-
-        int originalLeft = left;
-        int originalSame = sameCount;
-        while (left + 1 <= right && boxes[left] == boxes[left + 1]) {
-            left++;
-            sameCount++;
+    private int dfs(int[] boxes, int l, int r, int k, int[][][] dp) {
+        if (l > r) {
+            return 0;
         }
 
-        int answer = (sameCount + 1) * (sameCount + 1) + dfs(left + 1, right, 0);
+        if (dp[l][r][k] != 0) {
+            return dp[l][r][k];
+        }
 
-        for (int mid = left + 1; mid <= right; mid++) {
-            if (boxes[mid] == boxes[left]) {
-                answer = Math.max(answer, dfs(left + 1, mid - 1, 0) + dfs(mid, right, sameCount + 1));
+        while (l < r && boxes[r] == boxes[r - 1]) {
+            r--;
+            k++;
+        }
+
+        dp[l][r][k] = dfs(boxes, l, r - 1, 0, dp) + (k + 1) * (k + 1);
+
+        for (int i = l; i < r; i++) {
+            if (boxes[i] == boxes[r]) {
+                dp[l][r][k] = Math.max(
+                    dp[l][r][k],
+                    dfs(boxes, l, i, k + 1, dp) + dfs(boxes, i + 1, r - 1, 0, dp)
+                );
             }
         }
 
-        memo[originalLeft][right][originalSame] = answer;
-        return answer;
+        return dp[l][r][k];
     }
 }
