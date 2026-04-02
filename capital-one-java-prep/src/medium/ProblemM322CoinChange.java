db6e98884316e0b0495ae2981d81e5141635140a
learnerkartey <162529793+learnerkartey@users.noreply.github.com>
2026-04-01 20:00:19 -0400

    Refactor coinChange method for clarity and efficiency

--- capital-one-java-prep/src/medium/ProblemM322CoinChange.java
+++ capital-one-java-prep/src/medium/ProblemM322CoinChange.java
@@ -1,14 +1,13 @@
-import java.util.*;
-
-public class ProblemM322CoinChange {
+class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
+         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
-
-        for (int coin : coins) {
-            for (int value = coin; value <= amount; value++) {
-                dp[value] = Math.min(dp[value], dp[value - coin] + 1);
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
             }
         }
         return dp[amount] > amount ? -1 : dp[amount];
